828c45a83bceaf76d878a2a4fc85d220525a7545
Sam Warwick <sam@samwarwick.com>
2024-10-23 19:02:58 +1100

    Add Amoy and Sepolia. Remove Mumbai, Rinkeby and Ropsten

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,52 +1,47 @@
 require('dotenv').config();
 
-const { ALCHEMY_MUMBAI_URL, ALCHEMY_RINKEBY_URL, ALCHEMY_ROPSTEN_URL, PUBLIC_KEY, PRIVATE_KEY } = process.env;
+const { ALCHEMY_AMOY_URL, ALCHEMY_SEPOLIA_URL, PUBLIC_KEY, PRIVATE_KEY } = process.env;
 
-const MUMBAI_CONTRACT = '0xECBc217b13dEA841c33b5b68c9c98041C2136205';
-const RINKEBY_CONTRACT = '0x5c18191C30a1fECb95c77E6160FbfcbB3aE9e456';
-const ROPSTEN_CONTRACT = '0x94d225df09E4C1B77b3e426DdAC710e5697e4D22';
+const AMOY_CONTRACT = '0xEa864D1dC30398382E86ba836e8FbBfc6A48fa59';
+const SEPOLIA_CONTRACT = '0xEa864D1dC30398382E86ba836e8FbBfc6A48fa59';
 
-const BASE_URI = "https://gateway.pinata.cloud/ipfs/Qmdh88hxWq78idcGuKa5qLg3Dixpht86EFsHTTdrq7wAqo";
-const NFT_IDS = [ "main", "sun", "night", "red-door", "white" ];
+const BASE_URI = 'https://gateway.pinata.cloud/ipfs/Qmdh88hxWq78idcGuKa5qLg3Dixpht86EFsHTTdrq7wAqo';
+const NFT_IDS = ['main', 'sun', 'night', 'red-door', 'white'];
 
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
 
-const contract = require("../artifacts/contracts/CastleNFT.sol/CastleNFT.json");
+const contract = require('../artifacts/contracts/CastleNFT.sol/CastleNFT.json');
 
 async function mintNFT(tokenURI) {
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
-  console.log(`nonce: ${nonce}`);
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+    console.log(`nonce: ${nonce}`);
 
-  const tx = {
-    'from': PUBLIC_KEY,
-    'to': contractAddress,
-    'nonce': nonce,
-    'gas': 500000,
-    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
-  };
+    const tx = {
+        from: PUBLIC_KEY,
+        to: contractAddress,
+        nonce: nonce,
+        gas: 500000,
+        data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+    };
 
-  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
-  signPromise
-    .then((signedTx) => {
-      web3.eth.sendSignedTransaction(
-        signedTx.rawTransaction,
-        function (err, hash) {
-          if (!err) {
-            console.log(
-              "The hash of your transaction is:", hash,
-              "\nCheck Alchemy's Mempool to view the status of your transaction!"
-            )
-          } else {
-            console.log(
-              "Something went wrong when submitting your transaction:", err
-            )
-          }
-        }
-      )
-    })
-    .catch((err) => {
-      console.log("Promise failed:", err)
-    })
+    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    signPromise
+        .then((signedTx) => {
+            web3.eth.sendSignedTransaction(signedTx.rawTransaction, function (err, hash) {
+                if (!err) {
+                    console.log(
+                        'The hash of your transaction is:',
+                        hash,
+                        "\nCheck Alchemy's Mempool to view the status of your transaction!",
+                    );
+                } else {
+                    console.log('Something went wrong when submitting your transaction:', err);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log('Promise failed:', err);
+        });
 }
 
 const argv = process.argv.slice(2);
@@ -58,22 +53,18 @@ if (argv.length !== 2) {
 let web3;
 let contractAddress;
 
-switch(argv[0]) {
-  case 'mumbai':
-    web3 = createAlchemyWeb3(ALCHEMY_MUMBAI_URL);
-    contractAddress = MUMBAI_CONTRACT; 
-    break;
-  case 'rinkeby':
-    web3 = createAlchemyWeb3(ALCHEMY_RINKEBY_URL);
-    contractAddress = RINKEBY_CONTRACT; 
-    break;
-  case 'ropsten':
-    web3 = createAlchemyWeb3(ALCHEMY_ROPSTEN_URL);
-    contractAddress = ROPSTEN_CONTRACT; 
-    break;
-  default:
-    console.log(`Unsupported network -- ${argv[0]}`)
-    process.exit(1);
+switch (argv[0]) {
+    case 'amoy':
+        web3 = createAlchemyWeb3(ALCHEMY_AMOY_URL);
+        contractAddress = AMOY_CONTRACT;
+        break;
+    case 'sepolia':
+        web3 = createAlchemyWeb3(ALCHEMY_SEPOLIA_URL);
+        contractAddress = SEPOLIA_CONTRACT;
+        break;
+    default:
+        console.log(`Unsupported network -- ${argv[0]}`);
+        process.exit(1);
 }
 
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
